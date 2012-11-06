771cec61f93d5bb456e7738445808d6f8329000e
Christoph Koke <koke@kip.uni-heidelberg.de>
2012-11-06 10:26:08 +0100

    made bootstrap.mk also downloading waf

--- playground/dependencies/remote/bootstrap.mk
+++ playground/dependencies/remote/bootstrap.mk
@@ -2,11 +2,18 @@ MODULES_DIR=modules
 DB_FILE=repos.json
 DB_URL=file://$(PWD)/remote/$(DB_FILE)
 
+WAF_FILE=waf
+WAF_URL=file://$(PWD)/../../$(WAF_FILE)
+
 main:
 	mkdir -p $(MODULES_DIR)
 	$(CURL) -o $(MODULES_DIR)/$(DB_FILE) $(DB_URL)
+	$(CURL) -o $(WAF_FILE) $(WAF_URL)
+	chmod +x $(WAF_FILE)
 	@cat $(MODULES_DIR)/$(DB_FILE)
 
 clean:
+	rm -f $(MODULES_DIR)/$(DB_FILE)
+	rm -f $(WAF_FILE)
 
 .PHONY: main
