7c3c7ad2b14c25588ce5836a6bd35e197131cab9
Christoph Koke <koke@kip.uni-heidelberg.de>
2012-12-14 16:34:34 +0100

    add build script for specialiced waf

---
+++ make_symwaf2ic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TOOLS="compat15,boost,test_base,gtest,openmp,mr,symwaf2ic,symap2ic_doxygen"
+# Note take care to preseve the leading tab character in the following line!
+PRELUDE="	from waflib.extras.symwaf2ic import prelude; prelude()"
+
+./waf-light distclean
+./waf-light "--tools=${TOOLS}" "--prelude=${PRELUDE}" configure build
+
