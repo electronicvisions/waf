electronicvisions/waf

commit e5cd63a670a0420d9ac3c419f9bf513ac97087ae

Issue 1320, do not propagate external libraries uselib vars

waflib/Tools/ccroot.py
@@ -248,7 +248,9 @@ def use_rec(self, name, **kw):
 
 	p = self.tmp_use_prec
 	for x in self.to_list(getattr(y, 'use', [])):
-		try:
+		if self.env["STLIB_" + x]:
+			continue
+ 		try:
 			p[x].append(name)
 		except KeyError:
 			p[x] = [name]

commit edf26656ab5fa925835d69c3799525815b3d109e

Tools: ccroot: fix typo introduced in e5cd63a6

waflib/Tools/ccroot.py
@@ -250,7 +250,7 @@ def use_rec(self, name, **kw):
 	for x in self.to_list(getattr(y, 'use', [])):
 		if self.env["STLIB_" + x]:
 			continue
- 		try:
+		try:
 			p[x].append(name)
 		except KeyError:
 			p[x] = [name]