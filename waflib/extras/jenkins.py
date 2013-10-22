0c40a362c9ce38ed14964477f8230b1e460ca360
Kai Husmann <kai.husmann@kip.uni-heidelberg.de>
2013-10-22 12:30:55 +0200

    jenkins mail trigger now available

--- waflib/extras/jenkins.py
+++ waflib/extras/jenkins.py
@@ -5,7 +5,7 @@
 # Integrates jenkins commands into symap2ic waf (symap2ic/src/jenkins)
 
 from waflib import Logs, Context, Options
-import os, re
+import os, re, subprocess
 
 TOOL            = "Jenkins Integration"
 
@@ -241,8 +241,9 @@ class JenkinsContext(Context.Context):
     jenkins_workspace   = None # options changes this value if WORKSPACE and JOB_NAME is available
     jenkins_job_name    = None
 
-    exitcode_build      = 1 # crash also leads to build
-    exitcode_dontBuild  = 0
+    exitcode_doNotBuild         = 0     # normal execution -> no build, we need to turn around the exitcode in the jenkins build trigger script
+    exitcode_build              = 1     # crash also leads to build, thats why we use non-zero to indicate a build
+    exitcode_buildDueFailure    = 101   # something unexpected, trigger a build in the hope of a jenkins admin registering it...
 
     detailed_doc ="""\
 The ./waf jenkins command only works in a Jenkins environment. The environment
@@ -284,6 +285,28 @@ Finally add an "Editable Email Notification" to the job (if mailtrigger was used
         else:
             return True # continue normal execution...
 
+
+    def minimal_symwaf2ic_test(self):
+        """returns False if no symwaf2ic checkout was found"""
+
+        symwaf2ic_ok = os.path.isdir(".symwaf2ic") and os.path.isfile(".symwaf2ic.conf.json") and os.path.isfile(".symwaf2ic.repo.conf")
+        if not symwaf2ic_ok:
+            Logs.warn("jenkins trigger: symwaf2ic incomplete --> build")
+            return False
+        #else
+        return True
+
+    def fetch_upstream(self):
+        """Fetches upstream changes (git fetch) and returns False if an error occurs"""
+        try:
+            self.cmd_and_log(['./waf', 'repos-fetch'], quiet=Context.BOTH)
+        except Exception as e:
+            Logs.error("Fetching origin updates failed --> build due failure!")
+            print(e.stdout, e.stderr)
+            return False
+        # else:
+        return True
+
     def sb_trigger(self, rargs):
         """\
 Simple build trigger
@@ -291,28 +314,264 @@ Simple build trigger
 This trigger just checks for upstream changes in the active project (head -1 repo.conf).
         """
 
-        # minimal symap2ic test
-        symwaf2ic_ok = os.path.isdir(".symwaf2ic") and os.path.isfile(".symwaf2ic.conf.json") and os.path.isfile(".symwaf2ic.repo.conf")
-        if not symwaf2ic_ok:
-            Logs.warn("jenkins trigger: symwaf2ic incomplete --> build")
+        if not self.minimal_symwaf2ic_test():
             return self.exitcode_build
-
-        # fetch origin
-        errcode = self.exec_command('./waf repos-fetch')
-        #errcode = self.exec_command('./waf mr-run "git fetch --no-progress"')
-        #errcode = self.exec_command('./waf mr-run git fetch --no-progress') # this should work
-        if errcode:
-            Logs.error("Fetching origin updates failed!")
-            return self.exitcode_build
+        if not self.fetch_upstream():
+            return self.exitcode_buildDueFailure
 
         # check status
         errcode = self.exec_command('./waf mr-run git status 2>/dev/null | grep "^# Your branch is behind \'.*\'.*$"')
         if errcode: # grep failed
             print "No changes found!"
-            return self.exitcode_dontBuild
+            return self.exitcode_doNotBuild
         else:
             print "Changes found!"
             return self.exitcode_build
 
         assert False # unreachable code
 
+    def sb_mailtrigger(self, rargs):
+        """
+Build trigger with blame-mail support
+
+This trigger fetches all upstream changes and compares the commits with the
+local state. The authors of the upstream changes are saved to a file named authors.
+This file can later be used - in cooperation with mail-ext-plugin - to send
+blame mails to the vandals (i.e. those who probably broke the build).
+        """
+
+        print
+        Logs.info("Executing Symwaf2ic Jenkins MailTrigger")
+
+        print # make output a bit human readable..
+        Logs.info("Acquiring data...")
+        if not self.minimal_symwaf2ic_test():
+            return self.exitcode_buildDueFailure
+        if not self.fetch_upstream():
+            return self.exitcode_buildDueFailure
+
+        fn_authors      = self.jenkins_workspace.make_node("authors").abspath()
+        fn_changelog    = self.jenkins_workspace.make_node("changelog").abspath()
+        curdir          = os.getcwd()
+
+        #origin_commits,fi = parseLog(cmd_fetch_log)
+        #checkout_commits,fi = parseLog(cmd_checkout_log)
+
+        logformat='-n1 --pretty=oneline'
+        cmd = (
+                "./waf",
+                "mr-xrun",
+                "--",
+                "ref=`git symbolic-ref -q HEAD` # refs/heads/<branchname>",
+                #"# upstream: The name of a local ref which can be considered “upstream” from the displayed ref (KHS: ie, origin)",
+                "branch=`git for-each-ref --format='%(upstream:short)' $ref` # origin/<branchname>",
+                "git log {logformat} $branch".format(logformat=logformat)
+        )
+        origin_commits,fi = parseLog(cmd,curdir)
+
+        cmd = "./waf mr-run -- git log {logformat}".format(logformat=logformat).split()
+        local_commits,fi = parseLog(cmd,curdir)
+
+        # assert dependencies (both commands should return the same set of repositories)
+        okeys = set(origin_commits.keys())
+        lkeys = set(local_commits.keys())
+        assert okeys == lkeys
+        del okeys, lkeys
+
+        # do some initial cleanup, if these files should be kept the build script must
+        # copy them to some log folder
+        if os.path.isfile(fn_authors): os.remove(fn_authors)
+        if os.path.isfile(fn_changelog): os.remove(fn_changelog)
+
+        # HERZ_AUS_GOLD: find changes, authors and the changelog
+        build_required=False
+
+        print # make output a bit human readable..
+        Logs.info("Looking for changes...")
+        for repo in origin_commits:
+            local = local_commits[repo]
+            origin = origin_commits[repo]
+
+            if local == origin:
+                print "Unchanged:", origin.shortstring()
+                continue
+
+            # else:
+            build_required=True
+            print "Changed:  ", local.getCommitRange(origin, nice=True)
+
+            with open(fn_changelog, "a") as f:
+                f.write("######################################################################\n")
+                f.write("# %66s #\n\n" % local.getCommitRange(origin, nice=True))
+
+            # retrieve authors and remove redundant entries (multiple commits)
+            os.chdir(os.path.join(curdir, repo))
+            p = ProcessPipe('git', 'log', local.getCommitRange(origin))
+            p.add( 'tee', '-a', fn_changelog )
+            p.add( 'grep', '^Author:' )
+            p.add( 'sort' )
+            p.add( 'uniq' )
+            p.add( 'sed', 's/^Author/To/' )
+            p.add( 'tee', '-a', fn_authors ) # we append from each repo
+            # print p # the pipe...
+            p = p.execute() # returns the final process (Popen)
+
+            #for line in p.stdout: print line,
+            p.wait()
+
+            # end changelog with a newline to separate it from the following one.
+            with open(fn_changelog, "a") as f:
+                f.write('\n')
+        # done: for repo in origin_commits
+        print # make output a bit human readable..
+
+
+        # handle build/ no build
+        if not build_required:
+            assert( not os.path.isfile(fn_authors) )
+            print "Mailman did not find any changes, no build required."
+            return self.exitcode_doNotBuild
+
+        # else: CHANGES HAVE BEEN FOUND
+
+        # check for an empty authors file, which indicates that local commits were
+        # found by the above routine -- in Jenkins environment this shouldn't happen
+        if ( os.path.getsize(fn_authors) == 0 ):
+            os.remove(fn_authors)
+            os.remove(fn_changelog)
+            Logs.error("""
+            WARNING: local commits found!
+            # The mailman build trigger is supposed to be used with unedited
+            # checkouts only, i.e. for automatic build tools like Jenkins.
+            # Triggering a //failure//-build!
+            """)
+            return self.exitcode_buildDueFailure
+
+        # else: REAL CHANGES HAVE BEEN FOUND
+
+        # cleanup (remove redundant authors (commiters to multiple repositories))
+        os.chdir(self.jenkins_workspace.abspath())
+        p = ProcessPipe('cat', fn_authors)
+        p.add( 'sort' )
+        p.add( 'uniq' )
+        p.add( 'tee', fn_authors)
+        p = p.execute()
+
+        Logs.info("The //potential// vandals:")
+        for line in p.stdout: print line,
+        print
+        p.wait()
+
+
+        # say goodby
+        assert( os.path.isfile(fn_authors) )    # must have been created at this point!
+        assert( os.path.isfile(fn_changelog) )  # like above
+
+        Logs.info("""
+            Beware, the vandals responsible for this build are known.
+            Pray that you did not introduce a regression!
+
+            Now triggering a build!
+            """
+        )
+        return self.exitcode_build
+
+### A CommitSpecifier instance holds a specific commit, ie. a repo name
+# (directory) and a commit id (hash) as well as the commit text class
+class CommitSpecifier():
+    def __init__(self, repo, commitId, commitText ):
+        assert( repo.startswith('./') ) # WORKSPACE relative path TODO: test -d check?
+        int(commitId, 16)               # must be a hexstring
+
+        self.repo       = repo
+        self.commitId   = commitId
+        self.commitText = commitText
+
+    def shortstring(self):
+        return '<%s, %s>' % (self.repo, self.commitId[0:8])
+
+    def getCommitRange(self, newer, nice=False):
+        assert self.repo == newer.repo, 'Different repos: "%s" != "%s"' % (self.repo, newer.repo)
+
+        if nice:
+            return "<%s, %s..%s>" % (self.repo, self.commitId[0:8], newer.commitId[0:8])
+        return '%s..%s' % (self.commitId, newer.commitId)
+
+    def __str__(self):
+        return 'Repo: %s\nId:   %s\nText: %s' % (self.repo, self.commitId, self.commitText)
+
+    def __eq__(self, other):
+        assert self.repo == other.repo, 'Different repos: "%s" != "%s"' % (self.repo, other.repo)
+
+        if self.commitId == other.commitId:
+            assert self.commitText == other.commitText
+            return True
+        return False
+
+
+### ProcessPipe helps building and executing process pipes without shell-pipe usage
+# useful to execute things like "git log | grep | sort | uniq", i.e process pipes...
+# does not need shell access!
+class ProcessPipe():
+    def __init__(self, *comargs):
+        self.commandStack = []
+        self.initial = comargs
+
+    def add(self, *comargs):
+        self.commandStack.append(comargs)
+
+    def execute(self):
+        p = subprocess.Popen(self.initial, stdout=subprocess.PIPE, shell=False)
+        for command in self.commandStack:
+            p = subprocess.Popen( command, stdin=p.stdout, stdout=subprocess.PIPE, shell=False )
+        return p
+
+    def __str__(self):
+        j =' '
+        ret = j.join(self.initial)
+        for command in self.commandStack:
+            ret = ret + " | " + j.join(command)
+        return ret
+
+
+### function to parse "git log -n1 --pretty=oneline", retrieving the recent authors
+def parseLog(command, toplevel):
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    # mr title line has been removed (was available in the old flow)
+    triple=[]   # 3 lines log output
+    ret={}      # ret['<repo>'] = CommitSpecifier(...)
+    for line in proc.stdout:
+        #print i, line,
+        #i+=1
+        triple.append(line)
+        if len(triple)==3:
+
+            # triple[0]                 # mr run: </path/to/repository>\n
+            s=triple[0].split()
+            assert( len(s) == 3 )
+            assert( s[0]+s[1] == 'mrrun:' ) # startswith "mr run:"
+            assert( s[2].startswith(toplevel) )  # is an absolute path to somedir in symap2ic/below toplevel
+
+            repo=s[2].replace(toplevel,".") # abspath to relpath (beutify)
+
+            # triple[1]                 # <hexstring> <commit message with spaces>\n
+            s=triple[1].partition(' ')
+            int(s[0], 16)                   # throws if not a hex string
+            assert( s[1] == ' ' )           # space found!
+
+            commitId=s[0]
+            commitText=s[2].strip()         # remove trailing newline
+
+            # triple[2]                 # \n
+            assert( triple[2] == "\n" )     # just a newline
+
+            c = CommitSpecifier(repo, commitId, commitText)
+            assert repo not in ret
+            ret[repo] = c
+            triple = []
+
+    assert( len(triple) == 1 )          # ~ "mr run: finished (N ok)"
+    assert( triple[0].startswith("mr run: finished (") )    # failures not jet handled..
+    final = triple[0]
+
+    return ret, final
