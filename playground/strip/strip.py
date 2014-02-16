ab82dd8b8b15f61c1930848703b198e59cca6497
Thomas Nagy <tnagy2pow10@gmail.com>
2014-02-16 23:42:47 +0100

    possible race when stripping libraries

--- playground/strip/strip.py
+++ playground/strip/strip.py
@@ -21,12 +21,13 @@ from waflib import Task, TaskGen
 class strip(Task.Task):
 	run_str = '${STRIP} ${SRC}'
 	color   = 'BLUE'
+	after   = ['cprogram', 'cxxprogram', 'cshlib', 'cxxshlib', 'fcprogram', 'fcshlib']
 
 @TaskGen.feature('strip')
 @TaskGen.after('apply_link')
 def add_strip_task(self):
 	try:
 		link_task = self.link_task
-	except:
+	except AttributeError:
 		return
-	tsk = self.create_task('strip', self.link_task.outputs[0])
+	self.create_task('strip', link_task.outputs[0])
