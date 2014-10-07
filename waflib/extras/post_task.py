e3a2e0e1c1bc27dcee5d291d800a3ea8d81e01ff
Christoph Koke <christoph.koke@kip.uni-heidelberg.de>
2014-10-07 15:51:24 +0200

    redesign post_task feature
    task are now collected and posted after the regular tasks have been posted.

--- waflib/extras/post_task.py
+++ waflib/extras/post_task.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env python
 # encoding: utf-8
-# Christoph Koke, 2013
+# Christoph Koke, 2013-2014
 
-from waflib.TaskGen import task_gen, feature, after_method
-
-
-def _patch_task_gen_post():
-    old_post = task_gen.post
-
-    def post(self):
-        if old_post(self):
-            for tsk in getattr(self, 'post_on_post', []):
-                tsk.post()
-            return True
-        else:
-            return False
-
-    task_gen.post = post
+import functools
+from waflib import Build
+from waflib.TaskGen import feature, after_method
 
 
-_patch_task_gen_post()
+def _patch():
+    """Update the BuildContext"""
+    old_post_group = Build.BuildContext.post_group
+
+    @functools.wraps(Build.BuildContext.post_group)
+    def post_group(self):
+        old_post_group(self)
+
+        post_tasks = getattr(self, 'post_task_task_list', [])
+        for tsk, post_task in post_tasks:
+            if getattr(tsk, 'posted', False):
+                for tsk in post_task:
+                    tsk.post()
+
+    Build.BuildContext.post_group = post_group
+
+
+_patch()
 
 
 @feature('post_task')
 @after_method('process_use')
 def add_manual_depencies(self):
-    self.post_on_post = getattr(self, 'post_on_post', [])
-    post_tasks = set(self.to_list(getattr(self, 'post_task', [])))
-    for dep in post_tasks:
-        dep_task_gen = self.bld.get_tgen_by_name(dep)
-        self.post_on_post.append(dep_task_gen)
+    post_tasks = getattr(self.bld, 'post_task_task_list', [])
+    post_task = set(self.to_list(getattr(self, 'post_task', [])))
+    post_tasks.append((self,
+                       [self.bld.get_tgen_by_name(dep) for dep in post_task]))
+    self.bld.post_task_task_list = post_tasks
