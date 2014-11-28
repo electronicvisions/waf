f3320a4b074f222c3276714568a1892c30b237d8
Jérôme Carretero <cJ@zougloub.eu>
2014-11-28 13:21:46 -0500

    extras: cfg_altoptions: use option group

--- waflib/extras/cfg_altoptions.py
+++ waflib/extras/cfg_altoptions.py
@@ -42,7 +42,8 @@ def name_to_dest(x):
 def options(opt):
 	def x(opt, param):
 		dest = name_to_dest(param)
-		opt.add_option('--%s-root' % dest,
+		gr = opt.get_option_group("configure options")
+		gr.add_option('--%s-root' % dest,
 		 help="path containing include and lib subfolders for %s" \
 		  % param,
 		)
