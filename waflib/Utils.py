47cf5cecaa4f1be287cf3be70e57f764e02935f2
Thomas Nagy <tnagy2pow10@gmail.com>
2015-04-03 21:24:46 +0200

    Issue 1552 - attempt to fix the installation path regression on 32-bit Ubuntu caused by Issue 1536

--- waflib/Utils.py
+++ waflib/Utils.py
@@ -725,11 +725,8 @@ def get_registry_app_path(key, filename):
 def lib64():
 	# default settings for /usr/lib
 	if os.sep == '/':
-		is_64 = platform.architecture()[0] == '64bit'
-		if os.path.exists('/usr/lib64') and not os.path.exists('/usr/lib32'):
-			# redhat
-			return '64' if is_64 else ''
-		else:
-			return '' if is_64 else '32'
+		if platform.architecture()[0] == '64bit':
+			if os.path.exists('/usr/lib64') and not os.path.exists('/usr/lib32'):
+				return '64'
 	return ''
 
