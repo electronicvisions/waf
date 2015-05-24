3886c709292baf4bdd4fda7b7ebded5261669221
Thomas Nagy <tnagy2pow10@gmail.com>
2015-05-24 15:39:46 +0200

    waf 1.8.10

--- waflib/Context.py
+++ waflib/Context.py
@@ -11,13 +11,13 @@ from waflib import Utils, Errors, Logs
 import waflib.Node
 
 # the following 3 constants are updated on each new release (do not touch)
-HEXVERSION=0x1080900
+HEXVERSION=0x1080a00
 """Constant updated on new releases"""
 
-WAFVERSION="1.8.9"
+WAFVERSION="1.8.10"
 """Constant updated on new releases"""
 
-WAFREVISION="06e49b2a82166aeb14dde8357c58387f252fc722"
+WAFREVISION="ea59b302c53aa18adf7c7048f9e31281dac0e934"
 """Git revision when the waf version is updated"""
 
 ABI = 98
--- wscript
+++ wscript
@@ -10,7 +10,7 @@ To add a tool that does not exist in the folder compat15, pass an absolute path:
 """
 
 
-VERSION="1.8.9"
+VERSION="1.8.10"
 APPNAME='waf'
 REVISION=''
 
