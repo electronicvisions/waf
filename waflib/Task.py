6c34e2a654f54574a89f2e4f9cc6d38baddbaf8a
Thomas Nagy <tnagy@waf.io>
2015-11-20 17:15:33 +0100

    Force scanner functions to run after task failures - Issue 1660

--- waflib/Task.py
+++ waflib/Task.py
@@ -206,6 +206,7 @@ class TaskBase(evil):
 		# remove the task signature immediately before it is executed
 		# in case of failure the task will be executed again
 		try:
+			# TODO waf 1.9 - this breaks encapsulation
 			del self.generator.bld.task_sigs[self.uid()]
 		except KeyError:
 			pass
