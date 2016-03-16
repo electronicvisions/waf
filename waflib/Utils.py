b63ae9b2efb56a8101a9bca69446a5dad5e480a0
Thomas Nagy <tnagy@waf.io>
2016-03-16 22:28:50 +0100

    Code simplification

--- waflib/Utils.py
+++ waflib/Utils.py
@@ -140,12 +140,8 @@ class lru_cache(ordered_iter_dict):
 				self.popitem(last=False)
 		ordered_iter_dict.__setitem__(self, key, value)
 	def __getitem__(self, key):
-		try:
-			ret = ordered_iter_dict.__getitem__(self, key)
-		except KeyError:
-			raise
-		else:
-			ordered_iter_dict.__setitem__(self, key, ret)
+		ret = ordered_iter_dict.__getitem__(self, key)
+		ordered_iter_dict.__setitem__(self, key, ret)
 		return ret
 
 is_win32 = os.sep == '\\' or sys.platform == 'win32' # msys2
