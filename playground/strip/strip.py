e2b415f974f62e7a99a9c405aaab737056212fad
Thomas Nagy <tnagy@waf.io>
2016-08-13 18:51:55 +0200

    Strip binaries

--- playground/strip/strip.py
+++ playground/strip/strip.py
@@ -26,31 +26,25 @@ copy_fun on the installation context::
 	Build.InstallContext.copy_fun = copy_fun
 """
 
+from waflib import Task
+
 def configure(conf):
 	conf.find_program('strip')
 
-from waflib import Task, TaskGen
-class strip(Task.Task):
-	run_str = '${STRIP} ${SRC}'
-	color   = 'BLUE'
-	before  = ['inst']
+def wrap_compiled_task(classname):
+	# override the class to add a new 'run' method
+	# such an implementation guarantees that the absence of race conditions
+	#
+	def run_all(self):
+		if self.env.NO_STRIPPING:
+			return cls1.run(self)
+		return cls1.run(self) or cls2.run(self)
 
-@TaskGen.feature('strip')
-@TaskGen.after('apply_link')
-def add_strip_task(self):
-	try:
-		link_task = self.link_task
-	except AttributeError:
-		return
-	self.create_task('strip', link_task.outputs[0])
+	cls1 = Task.classes[classname]
+	cls2 = type(classname, (cls1,), {'run_str': '${STRIP} ${TGT[0].abspath()}'})
+	cls3 = type(classname, (cls2,), {'run': run_all})
 
-@TaskGen.feature('cshlib', 'cxxshlib', 'cprogram', 'cxxprogram', 'fcprogram', 'fcshlib')
-@TaskGen.after('process_use', 'apply_link')
-def set_strip_order(self):
-	# as an example
-	if getattr(self, 'link_task', None):
-		for x in self.tmp_use_seen:
-			tg = self.bld.get_tgen_by_name(x)
-			if getattr(tg, 'link_task', None):
-				self.link_task.set_run_after(tg.link_task)
+for k in 'cprogram cshlib cxxprogram cxxshlib fcprogram fcshlib'.split():
+	if k in Task.classes:
+		wrap_compiled_task(k)
 
