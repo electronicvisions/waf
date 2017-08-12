28846d6f34d998847a6b969313a8904bdc8cdaa8
Thomas Nagy <tnagy@waf.io>
2017-08-12 16:31:51 +0200

    Remove one Node class assumption in Task.py
    Tasks cannot assume that additional dependency (deps_man) belong
    to the same Node object class before obtaining their signatures

--- waflib/Task.py
+++ waflib/Task.py
@@ -710,10 +710,11 @@ class Task(evil):
 					continue
 
 				for v in d:
-					if isinstance(v, bld.root.__class__):
+					try:
 						v = v.get_bld_sig()
-					elif hasattr(v, '__call__'):
-						v = v() # dependency is a function, call it
+					except AttributeError:
+						if hasattr(v, '__call__'):
+							v = v() # dependency is a function, call it
 					upd(v)
 
 	def sig_vars(self):
