f043a94a02ece47c081debd6689d48011842fef7
Calle Rosenquist <xbreak@users.noreply.github.com>
2017-09-04 09:10:58 +0000

    pytest: Added better support for normal and pyext library dependencies. This also includes the `buildcopy` feature that allows construction of complete Python packages in the build dir.

--- playground/pytest/src/foo/foo.py
+++ playground/pytest/src/foo/foo.py
@@ -2,7 +2,14 @@
 # encoding: utf-8
 # Calle Rosenquist, 2016 (xbreak)
 
-def sum(a, b):
+import os
 
+# Import the foo extension shared object
+from foo import foo_ext
+
+def sum(a, b):
 	return a + b
+
+def ping():
+	return foo_ext.ping()
 
