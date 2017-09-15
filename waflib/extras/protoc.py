2474218874f04061e82318a3d26413f762804157
Federico Pellegrin <fede@evolware.org>
2017-09-15 20:52:08 +0200

    protoc: extend extra to support also Python language (#2028)
    * protoc: extend extra to support also Python language
    * protoc: fix ext_out, unite c++/python generation, fix protoc_includes name

--- waflib/extras/protoc.py
+++ waflib/extras/protoc.py
@@ -6,11 +6,12 @@
 import re
 from waflib.Task import Task
 from waflib.TaskGen import extension
+from waflib import Errors
 
 """
 A simple tool to integrate protocol buffers into your build system.
 
-Example::
+Example for C++:
 
     def configure(conf):
         conf.load('compiler_cxx cxx protoc')
@@ -19,8 +20,31 @@ Example::
         bld(
                 features = 'cxx cxxprogram'
                 source   = 'main.cpp file1.proto proto/file2.proto',
-                include  = '. proto',
+                includes = '. proto',
                 target   = 'executable')
+
+Example for Python:
+
+    def configure(conf):
+        conf.load('python protoc')
+
+    def build(bld):
+        bld(
+                features = 'py'
+                source   = 'main.py file1.proto proto/file2.proto',
+                protoc_includes  = 'proto')
+
+Example for both Python and C++ at same time:
+
+    def configure(conf):
+        conf.load('cxx python protoc')
+
+    def build(bld):
+        bld(
+                features = 'cxx py'
+                source   = 'file1.proto proto/file2.proto',
+                protoc_includes  = 'proto')	# or includes
+
 
 Notes when using this tool:
 
@@ -35,9 +59,9 @@ Notes when using this tool:
 """
 
 class protoc(Task):
-	run_str = '${PROTOC} ${PROTOC_FLAGS} ${PROTOC_ST:INCPATHS} ${SRC[0].bldpath()}'
+	run_str = '${PROTOC} ${PROTOC_FL:PROTOC_FLAGS} ${PROTOC_ST:INCPATHS} ${PROTOC_ST:PROTOC_INCPATHS} ${SRC[0].bldpath()}'
 	color   = 'BLUE'
-	ext_out = ['.h', 'pb.cc']
+	ext_out = ['.h', 'pb.cc', '.py']
 	def scan(self):
 		"""
 		Scan .proto dependencies
@@ -47,9 +71,17 @@ class protoc(Task):
 		nodes = []
 		names = []
 		seen = []
+		search_nodes = []
 
 		if not node:
 			return (nodes, names)
+
+		if 'cxx' in self.generator.features:
+			search_nodes = self.generator.includes_nodes
+
+		if 'py' in self.generator.features:
+			for incpath in getattr(self.generator, 'protoc_includes', []):
+				search_nodes.append(self.generator.bld.path.find_node(incpath))
 
 		def parse_node(node):
 			if node in seen:
@@ -60,7 +92,7 @@ class protoc(Task):
 				m = re.search(r'^import\s+"(.*)";.*(//)?.*', line)
 				if m:
 					dep = m.groups()[0]
-					for incnode in self.generator.includes_nodes:
+					for incnode in search_nodes:
 						found = incnode.find_resource(dep)
 						if found:
 							nodes.append(found)
@@ -73,14 +105,43 @@ class protoc(Task):
 
 @extension('.proto')
 def process_protoc(self, node):
-	cpp_node = node.change_ext('.pb.cc')
-	hpp_node = node.change_ext('.pb.h')
-	self.create_task('protoc', node, [cpp_node, hpp_node])
-	self.source.append(cpp_node)
+	incdirs = []
+	out_nodes = []
+	protoc_flags = []
 
-	if 'cxx' in self.features and not self.env.PROTOC_FLAGS:
+	if 'cxx' in self.features:
+		cpp_node = node.change_ext('.pb.cc')
+		hpp_node = node.change_ext('.pb.h')
+		self.source.append(cpp_node)
+		out_nodes.append(cpp_node)
+		out_nodes.append(hpp_node)
+
 		#self.env.PROTOC_FLAGS = '--cpp_out=%s' % node.parent.get_bld().abspath() # <- this does not work
-		self.env.PROTOC_FLAGS = '--cpp_out=%s' % node.parent.get_bld().bldpath()
+		protoc_flags.append('--cpp_out=%s' % node.parent.get_bld().bldpath())
+
+	if 'py' in self.features:
+		py_node = node.change_ext('_pb2.py')
+		self.source.append(py_node)
+		out_nodes.append(py_node)
+
+		protoc_flags.append('--python_out=%s' % node.parent.get_bld().bldpath())
+
+	if out_nodes:
+		self.create_task('protoc', node, out_nodes)
+		if not self.env.PROTOC_FLAGS:
+			self.env.PROTOC_FLAGS = protoc_flags
+	else:
+		raise Errors.WafError('Feature %s not supported by protoc extra' % self.features)
+
+
+	if isinstance(self.env.PROTOC_FLAGS, str):	# Backwards compatibility as run_str format changed
+		self.env.PROTOC_FLAGS = [ self.env.PROTOC_FLAGS ]
+
+	# Instruct protoc where to search for .proto included files. For C++ standard include files dirs are used,
+	# but this doesn't apply to Python / Java
+	for incpath in getattr(self, 'protoc_includes', []):
+		incdirs.append(self.bld.path.find_node(incpath).bldpath())
+	self.env.PROTOC_INCPATHS = incdirs
 
 	use = getattr(self, 'use', '')
 	if not 'PROTOBUF' in use:
@@ -90,4 +151,4 @@ def configure(conf):
 	conf.check_cfg(package="protobuf", uselib_store="PROTOBUF", args=['--cflags', '--libs'])
 	conf.find_program('protoc', var='PROTOC')
 	conf.env.PROTOC_ST = '-I%s'
-
+	conf.env.PROTOC_FL = '%s'
