b852e10b9ca7d84c03b708fc4d1d1ec2dd603bbb
Eric Müller <mueller@kip.uni-heidelberg.de>
2017-12-01 18:40:57 +0100

    Add cross_gxx tool for NUX C++ support
    Change-Id: I9c47905bd3ad013e32432d3ff77e162aa7efc3e4

---
+++ waflib/extras/cross_gxx.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+cross_gxx cross compiler detection.
+"""
+
+import os, sys
+from waflib import Configure, Options, Utils
+from waflib.Tools import ccroot, gxx
+from waflib.Configure import conf
+from waflib.extras import cross_ar, visionflags
+
+@conf
+def find_cross_gxx(conf):
+	"""
+	Find the cross compiler and if present, try to detect its version number
+	"""
+	cxx = conf.find_program('%s-g++' % conf.env.CROSS_PLATFORM, var='CXX')
+	conf.find_program('%s-ld' % conf.env.CROSS_PLATFORM, var='LINK_CXX')
+	conf.get_cc_version(cxx, gcc=True)
+	conf.env.CXX_NAME = '%s-gxx' % conf.env.CROSS_PLATFORM
+
+@conf
+def cross_gxx_common_flags(conf):
+	# Update some flags to be compatible with the separate linker, instead of
+	# using the cross compiler as the linker driver
+	conf.env['RPATH_ST']            = '-rpath,%s'
+	conf.env['SONAME_ST']           = '-h,%s'
+	conf.env['SHLIB_MARKER']        = '-Bdynamic'
+	conf.env['STLIB_MARKER']        = '-Bstatic'
+	conf.env['LINKFLAGS_cstlib']    = '-Bstatic'
+
+def options(opt):
+	opt.load('visionflags')
+
+def configure(conf):
+	"""
+	Configuration for cross_gcc
+	"""
+	conf.find_cross_gxx()
+	conf.find_cross_ar()
+	conf.gxx_common_flags()
+	conf.cross_gxx_common_flags()
+	# ECM: cf. gcc.py
+	conf.load("visionflags")
+	conf.cxx_load_tools()
--- waflib/extras/nux_compiler.py
+++ waflib/extras/nux_compiler.py
@@ -6,18 +6,37 @@ from waflib import Options
 
 def options(opt):
 	opt.load('cross_gcc')
+	opt.load('cross_gxx')
 	opt.load('nux_assembler')
 
 def configure(conf):
 	conf.env.CROSS_PLATFORM = Options.options.cross_prefix
 	conf.load('cross_gcc')
+	conf.load('cross_gxx')
 	conf.load('cross_as')
 	conf.env.CFLAGS += [
 		'-ffreestanding',
 		'-mcpu=nux',
 		'-std=gnu11',
+		#'-fstack-protector-strong', # requires handler
+		'-fno-common',
+		'-ffunction-sections',
+		'-fdata-sections',
+	]
+	conf.env.CXXFLAGS += [
+		'-ffreestanding',
+		'-mcpu=nux',
+		'-std=gnu++11',
+		'-fno-exceptions',
+		'-fno-rtti',
+		'-fno-non-call-exceptions',
+		#'-fstack-protector-strong', # requires handler
+		'-fno-common',
+		'-ffunction-sections',
+		'-fdata-sections',
 	]
 	conf.env.DEFINES += ['SYSTEM_HICANN_DLS_MINI']
 	conf.env.LINKFLAGS += [
 		'-T%s' % conf.path.find_node('libnux/elf32nux.x').abspath(),
+		#'--gc-sections', # removes too much ;)
 	]
