43039e716b9b293dcc17529eb520d673c2e4789b
Thomas Nagy <tnagy@waf.io>
2018-02-14 21:02:43 +0100

    Fix the apidocs LaTeX paper size parameter

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -488,10 +488,13 @@ htmlhelp_basename = 'wafdoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 # The paper size ('letter' or 'a4').
-#latex_paper_size = 'a4'
+latex_paper_size = 'a4'
 
 # The font size ('10pt', '11pt' or '12pt').
 #latex_font_size = '10pt'
+latex_elements = {
+   'papersize':'a4paper',
+}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
