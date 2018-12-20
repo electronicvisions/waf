c203615d048eb567a296b66c15d45eb83d011976
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2018-12-20 14:32:45 +0100

    Fixup 6812f71: Broken syntax for libnux flags
    Change-Id: Idd2aaed39b6f5ffa8bf5c23c918fa943bdff2d85

--- waflib/extras/visionflags.py
+++ waflib/extras/visionflags.py
@@ -172,7 +172,8 @@ def configure(conf):
 
 	# let's stick to bfd for nux cross compilation
         # PS (20-12-18): this prevents from using the powerpc compiler without using libnux (#3033)
-	if 'powerpc' in conf.env.CROSS_PLATFORM and 'LIBNUX_DLS_VERSION' in x for x in conf.env.DEFINES:
+	if 'powerpc' in conf.env.CROSS_PLATFORM and any(['LIBNUX_DLS_VERSION' in x
+	                                                 for x in conf.env.DEFINES]):
 		linker = 'bfd'
 	else:
 		linker = Options.options.linker
