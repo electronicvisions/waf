92e9764f585718a257d15866ea91c8ede277f1a7
Thomas Nagy <tnagy@waf.io>
2019-05-27 18:05:02 +0200

    Optimize sub process startup with workers

--- waflib/Utils.py
+++ waflib/Utils.py
@@ -885,7 +885,7 @@ def get_process():
 	except IndexError:
 		filepath = os.path.dirname(os.path.abspath(__file__)) + os.sep + 'processor.py'
 		cmd = [sys.executable, '-c', readf(filepath)]
-		return subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=0)
+		return subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=0, close_fds=True)
 
 def run_prefork_process(cmd, kwargs, cargs):
 	"""
