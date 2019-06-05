e6eca1eec5990023ecb5e297a646e333ddc523b3
fedepell <fede@evolware.org>
2019-06-05 14:59:26 +0200

    qt5 demo: add example of unit testing using QtTest integrated into waf with standard waf_unit_test Tool

--- demos/qt5/foo.cpp
+++ demos/qt5/foo.cpp
@@ -3,7 +3,24 @@
 #include "foo.h"
 
 Foo::Foo() : QWidget(NULL) {
+	m_button = new QPushButton("Foo Button", this);
+	m_button->setGeometry(QRect(QPoint(50, 60),
+	QSize(120, 50)));
+	connect(m_button, SIGNAL (released()), this, SLOT (handleButton()));
+	myToggle = true;
+}
 
+void Foo::handleButton() {
+	if (myToggle) {
+		m_button->setText("Button Foo");
+	} else {
+		m_button->setText("Foo Button");
+	}
+	myToggle = !myToggle;
+}
+
+int Foo::FortyTwo() {
+	return 42;
 }
 
 class Bar_private : public QWidget {
