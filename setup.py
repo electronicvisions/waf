56f3723a86585e5eff02a78b944f52883305b039
Adriano Scoditti <adriano.scoditti@arm.com>
2019-06-26 15:37:45 +0200

    Removing the incorrect License description

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setuptools.setup(
     scripts=['waf-light'],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
