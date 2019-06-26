dff204fad4b6967165c7da6a9eb5eb4d9ff347cc
Adriano Scoditti <adriano.scoditti@arm.com>
2019-06-26 15:51:48 +0200

    Move setup.py in subdir to unclutter the project root

--- utils/setup.py
+++ utils/setup.py
@@ -1,7 +1,9 @@
 import setuptools
+import sys
+sys.path.insert(0,'..')
 import waflib.Context
 
-with open("README.md", "r") as fh:
+with open("../README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -13,8 +15,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/ita1024/waf",
+    package_dir={'': '../'},
     packages=['waflib', 'waflib/Tools', 'waflib/extras'],
-    scripts=['waf-light'],
+    scripts=['../waf-light'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
