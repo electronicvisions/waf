electronicvisions/waf

commit 4ab69329ff6907d63af61ee324df69e4c38b2278

Add clone_depth option for repo db entries
Change-Id: I0f8cbfc5074b286e5d505237bc98b644ef328fc5

waflib/extras/mr.py
@@ -40,6 +40,12 @@ class Repo_DB(object):
     def get_init(self, name):
         return self.db[name].get("init_cmds",'')
 
+    def get_clone_depth(self, name):
+        clone_depth = self.db[name].get("clone_depth", None)
+        if clone_depth is not None and (clone_depth is 0 or clone_depth < -1):
+            raise ValueError("Clone depth from repo db {} not in valid range [-1, 1, 2, ...]".format(self.clone_depth))
+        return clone_depth
+
     def get_aliases(self, name):
         return self.db[name].get("aliases", [])
 
@@ -239,9 +245,9 @@ class GitProject(Project):
     def __init__(self, *args, **kw):
         super(self.__class__, self).__init__(*args, **kw)
 
-    def mr_checkout_cmd(self, base_node, url):
+    def mr_checkout_cmd(self, base_node, url, clone_depth):
         path = self.path_from(base_node)
-        depth = self.clone_depth
+        depth = clone_depth
         depth = '--depth {}'.format(depth) if depth >= 0 else ''
         cmd = ["git clone --branch '{branch}' {depth} '{url}' '{target}'".format(
             branch=self.required_branch, depth=depth,
@@ -370,6 +376,8 @@ class MR(object):
     def setup_repo_db(self, ctx, cfg, top, db_url, db_type):
         # first install some mock object that servers to create the repo db repository
         class MockDB(object):
+            def get_clone_depth(self, *k, **kw):
+                return None
             def get_url(self, *k, **kw):
                 return db_url
             def get_init(self, *k, **kw):
@@ -704,8 +712,14 @@ class MR(object):
             self.mr_print("Checking out repository %s {%s} to '%s'..."
                 % (self.db.get_url(p.name), p.required_branch, p.name), sep = '')
 
+        if self.clone_depth:
+            clone_depth = self.clone_depth
+        else:
+            db_clone_depth = self.db.get_clone_depth(p.name)
+            clone_depth = db_clone_depth if db_clone_depth else -1
+
         args = ['config', p.name,
-                p.mr_checkout_cmd(self.base, self.db.get_url(p.name))
+                p.mr_checkout_cmd(self.base, self.db.get_url(p.name), clone_depth)
                ]
         init_cmd = p.mr_init_cmd(self.db.get_init(p.name), self.gerrit_url)
         if init_cmd:

waflib/extras/symwaf2ic.py
@@ -203,9 +203,9 @@ def options(opt):
             default=None
             )
     gr.add_option(
-        "--clone-depth", dest="clone_depth", action="store",
-        type=int, help="To clone the full history use -1 [default is full history]",
-        default=-1
+            "--clone-depth", dest="clone_depth", action="store",
+            type=int, help="Git clone depth. If not given, first use depth given in repo db then fallback to full history",
+            default=None
     )
 
 
@@ -285,6 +285,8 @@ class MainContext(Symwaf2icContext):
         self.repo_db_url = cmdopts.repo_db_url
         self.repo_db_type = cmdopts.repo_db_type
         self.clone_depth= cmdopts.clone_depth
+        if self.clone_depth is not None and (self.clone_depth is 0 or self.clone_depth < -1):
+            raise ValueError("Provided clone depth argument {} not in valid range [-1, 1, 2, ...]".format(self.clone_depth))
         self.gerrit_url = cmdopts.gerrit_url
         self.gerrit_username = cmdopts.gerrit_username
 

commit 53464d63636391a61e51c7f5caa9cb42e238b53b

Fixup 4ab6932: Always deep-clone projects with a specific target ref
In 0aead28e, we introduced a force-deep-clone mechanism for projects
to be checked out with a specific reference – as mr will check out the
revision only after cloning. 4ab6932 introduced a bug, where this
mechanism is ignored. This change reinstantiates the behavior introduced
in 0aead28e.

Change-Id: I77f8b8c6464cca6cf05a99221b1a3cedaa8cc930

waflib/extras/mr.py
@@ -690,16 +690,6 @@ class Project(object):
             self._real_branch =  stdout.strip()
         return self._real_branch
 
-    @property
-    def clone_depth(self):
-        if self.ref:
-            Logs.warn("\nProject {project} is to be checked out at reference "
-                      "{ref}. Shallow clone omitted.".format(project=self.name,
-                                                             ref=self.ref))
-            return -1
-
-        return self._clone_depth
-
     def update_branch(self, force=False):
         cmd = self.reset_branch_cmd() if force else self.set_branch_cmd()
         ret, stdout, stderr = self.exec_cmd(cmd)
@@ -871,10 +861,18 @@ class GitProject(Project):
 
     def mr_checkout_cmd(self, base_node, url, clone_depth):
         path = self.path_from(base_node)
-        depth = clone_depth
-        depth = '--depth {}'.format(depth) if depth >= 0 else ''
+
+        depth_arg = '--depth {}'.format(clone_depth)
+        if clone_depth < 0:
+            depth_arg = ''
+        if self.ref:
+            Logs.warn("\nProject {project} is to be checked out at reference "
+                      "{ref}. Shallow clone omitted.".format(project=self.name,
+                                                             ref=self.ref))
+            depth_arg = ''
+
         cmd = ["git clone --branch '{branch}' {depth} '{url}' '{target}'".format(
-            branch=self.required_branch, depth=depth,
+            branch=self.required_branch, depth=depth_arg,
             url=url, target=os.path.basename(path))]
         return 'checkout=%s' % "; ".join(cmd)
 